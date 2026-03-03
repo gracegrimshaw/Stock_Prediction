28bab0109f943fa839e7c441795aee78f7867e62
gracegrimshaw <grace.e.grimshaw@tcu.edu>
2026-03-03 16:40:46 -0600

    Refactor imports and update prediction logic

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -131,7 +131,7 @@ def display_explanation(input_df, session, aws_bucket):
 
     st.subheader("🔍 Decision Transparency (SHAP)")
     fig, ax = plt.subplots(figsize=(10, 4))
-    shap.plots.waterfall(shap_values[0,:,0]), max_display=10)
+    shap.plots.waterfall(shap_values[0,:,0], max_display=10)
     st.pyplot(fig)
     # top feature   
     top_feature = pd.Series(shap_values[0, :, 0].values, index=shap_values[0, :, 0].feature_names).abs().idxmax()
@@ -170,6 +170,7 @@ if submitted:
         display_explanation(input_df,session, aws_bucket)
     else:
         st.error(res)
+
 
 
 
